866f57a43b4243e206aae7464575b49c444b96f0
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2023-11-21 08:29:21 +0530

    js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const PLAYERS = [
-  "Spiderman",
-  "Captain America",
-  "Wonderwoman",
+  'Spiderman',
+  'Captain America',
+  'Wonderwoman',
   // "Popcorn",
   // "Gemwoman",
   // "Bolt",
@@ -27,9 +27,9 @@ class Player {
     // Progression 1: Create member variables and assign values
     this.id = id;
     this.name = name;
-    this.strength = this.getRandomStrength();
-    this.image = `/images/super-${id}.png`;
-    this.type = type;
+    this.strength = this.getRandomStrength()
+    this.image = `/images/super-${id}.png`
+    this.type = type; 
   }
   // getting random strength
   getRandomStrength = () => {
@@ -40,25 +40,26 @@ class Player {
   view = () => {
     // Accumulate HTML template
     // Type your code here
-    let id = this.id + 1;
-    const player = document.createElement("div");
-    player.classList.add("player");
-    player.setAttribute("data-id", id);
-
-    const playerImage = document.createElement("img");
-    playerImage.setAttribute("src", `images/super-${id}.png`);
-
-    const playerName = document.createElement("div");
-    playerName.classList.add("name");
+    let id = this.id + 1
+    const player = document.createElement('div');
+    player.classList.add('player');
+    player.setAttribute('data-id', id);
+    
+    const playerImage = document.createElement('img');
+    playerImage.setAttribute('src', `images/super-${id}.png`);
+    
+    const playerName = document.createElement('div');
+    playerName.classList.add('name');
     playerName.textContent = this.name;
-
-    const playerStrength = document.createElement("div");
-    playerStrength.classList.add("strength");
+    
+    const playerStrength = document.createElement('div');
+    playerStrength.classList.add('strength');
     playerStrength.textContent = this.strength;
-
+    
     player.appendChild(playerImage);
     player.appendChild(playerName);
     player.appendChild(playerStrength);
+    
 
     return player;
   };
@@ -72,26 +73,21 @@ class Superwar {
     // Use Map method to loop through players argument and create new players
     // Type your code here
     this.players = players.map((item,index)=>{
-      let type;
-      if (index % 2 === 0) {
-        type = "hero";
-      } else {
-        type = "villain";
-      }
+      let type = (index%2==0)?"hero":"villain"
       return new Player(index,item,type)
     })
   }
 
   // Display players in HTML
   viewPlayers = () => {
-    let team = document.getElementById("heroes");
-    team.innerHTML = "";
-    let fragment = this.buildPlayers("hero");
+    let team = document.getElementById('heroes');
+    team.innerHTML = '';
+    let fragment = this.buildPlayers('hero');
     team.append(fragment);
 
-    team = document.getElementById("villains");
-    team.innerHTML = "";
-    fragment = this.buildPlayers("villain");
+    team = document.getElementById('villains');
+    team.innerHTML = '';
+    fragment = this.buildPlayers('villain');
     team.append(fragment);
   };
 
@@ -106,7 +102,7 @@ class Superwar {
 }
 
 // uncomment this part -- only after you complete progression 3
-// window.onload = () => {
-//     const superwar = new Superwar(PLAYERS);
-//     superwar.viewPlayers();
-// }
+window.onload = () => {
+    const superwar = new Superwar(PLAYERS);
+    superwar.viewPlayers();
+}
